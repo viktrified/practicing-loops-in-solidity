fe13a73b2765004d197c38f9f60da71c692d23ce
Victory Chidera Ezeokafor <victoryezeokafor@gmail.com>
2024-09-21 22:10:49 +0100

    nested for loops

--- ForLoops.sol
+++ ForLoops.sol
@@ -57,4 +57,27 @@ contract SeriousContract {
     }
 }
 
-contract TheLastFail {}
+contract LastFailMyAss {
+    // a for loop that nest a for loop that nest a while loop
+    uint256 public count;
+
+    function myOwnMethod() public returns (uint256) {
+        return count++;
+     }
+
+    function firstLoop() public returns (uint256) {
+        for (uint256 i = 0; i < 5; i++) {
+            for (uint256 j = 0; j < 5; j++) {
+                myOwnMethod();
+
+                uint256 k = 0;
+                do {
+                    myOwnMethod();
+
+                    k++;
+                } 
+                while (k < 5);
+            }
+        } return count;
+    }
+}
